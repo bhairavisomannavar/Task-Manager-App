38d19f556f328f24734078f1611c117e57557a95
Bhairavi <bhairavisomannavar@gmail>
2026-04-11 16:24:04 +0530

    updated backend url

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'http://localhost:8000';
+export const API_URL = 'https://task-manager-24193fc2d-bhairavisomannavar13-6047s-projects.vercel.app';
